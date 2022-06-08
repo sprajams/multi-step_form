b4d662a54bf630933bddf4e6231246e24fbe198b
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-08 11:40:03 -0700

    clean up and basic styling of page

--- src/components/Success/index.jsx
+++ src/components/Success/index.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import Details from "../Details";
+import styles from "./styles.module.scss";
+
 function Sucess({ response }) {
   const [open, setOpen] = useState(false);
   return (
-    <div>
-      <div>congrats!</div>
-      <button
-        onClick={() => {
-          setOpen(!open);
-        }}
-      >
-        details
-      </button>
-      {open ? <Details response={response} /> : null}
+    <div className={styles.outer}>
+      <div className={styles.greetingWrap}>
+        <div className={styles.greetingIcon}>✔️</div>
+        <h2 className={styles.greetingText}>Complete!</h2>
+      </div>
+
+      <div className={styles.detailWrap}>
+        <button
+          className={styles.detailBtn}
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          Show Details
+        </button>
+        <div className={styles.detailInfo}>
+          {open ? <Details response={response} /> : null}
+        </div>
+      </div>
     </div>
   );
 }
